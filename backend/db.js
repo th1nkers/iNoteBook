b39e50fe1f719a3a34af02e3a34541bd3e57c14a
thinkers121 <suryasingh11112003@gmail.com>
2023-02-18 12:50:49 +0530

    Express Server Setup

---
+++ backend/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const mongoURI = "mongodb+srv://thinkerssss:wCWeTuAnt3po64QY@cluster0.jeojreb.mongodb.net/?retryWrites=true&w=majority"
+
+const connectToMongo = () =>{
+    mongoose.connect(mongoURI, ()=>{
+        console.log("Connected to Mongo Successfully");
+    })
+}
+
+module.exports = connectToMongo; 
